ba00e7b712ec6da2c32205246ddbcb2b7b71ea71
arifusr <arifusr@gmail.com>
2021-04-07 20:17:55 +0700

    initial commit

---
+++ Extreme C.mm
@@ -0,0 +1,102 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Extreme C" FOLDED="false" ID="ID_1181607215" CREATED="1617801112136" MODIFIED="1617801114201" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="1" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Chapter 1: Essential Features" POSITION="right" ID="ID_1525379735" CREATED="1617801137855" MODIFIED="1617801138393">
+<edge COLOR="#ff0000"/>
+<node TEXT="Preprocessor directives" ID="ID_142647693" CREATED="1617801145394" MODIFIED="1617801145831">
+<node TEXT="Macros" ID="ID_1092118483" CREATED="1617801151347" MODIFIED="1617801151727">
+<node TEXT="Defining a macro" ID="ID_1697735114" CREATED="1617801164389" MODIFIED="1617801164839"/>
+<node TEXT="Variadic macros" ID="ID_207234980" CREATED="1617801171201" MODIFIED="1617801171488"/>
+<node TEXT="Advantages and disadvantages of macros" ID="ID_1817216187" CREATED="1617801189336" MODIFIED="1617801189639"/>
+</node>
+<node TEXT="Conditional compilation" ID="ID_1665237905" CREATED="1617801156153" MODIFIED="1617801156503"/>
+</node>
+<node TEXT="Variable pointers" ID="ID_1324582691" CREATED="1617801196561" MODIFIED="1617801197028">
+<node TEXT="Syntax" ID="ID_395729139" CREATED="1617801203534" MODIFIED="1617801203863"/>
+<node TEXT="Arithmetic on variable pointers" ID="ID_143130040" CREATED="1617801209014" MODIFIED="1617801209387"/>
+<node TEXT="Generic pointers" ID="ID_291177094" CREATED="1617801214497" MODIFIED="1617801214497"/>
+<node TEXT="Size of a pointer" ID="ID_1795618345" CREATED="1617801214694" MODIFIED="1617801224604"/>
+<node TEXT="Dangling pointers" ID="ID_481825174" CREATED="1617801229999" MODIFIED="1617801230301"/>
+</node>
+<node TEXT="Some details about functions" ID="ID_1293797079" CREATED="1617801245072" MODIFIED="1617801245490">
+<node TEXT="Anatomy of a function" ID="ID_1920454779" CREATED="1617801252732" MODIFIED="1617801253185"/>
+<node TEXT="Importance in design" ID="ID_849683750" CREATED="1617801257308" MODIFIED="1617801257623"/>
+<node TEXT="Stack management" ID="ID_815289170" CREATED="1617801261533" MODIFIED="1617801261863"/>
+<node TEXT="Pass-by-value versus pass-by-reference" ID="ID_839711089" CREATED="1617801265975" MODIFIED="1617801266270"/>
+</node>
+<node TEXT="Function pointers" ID="ID_1936839063" CREATED="1617801378960" MODIFIED="1617801379353"/>
+<node TEXT="Structures" ID="ID_1653122799" CREATED="1617801387627" MODIFIED="1617801387940">
+<node TEXT="Why structures?" ID="ID_677100195" CREATED="1617801395501" MODIFIED="1617801395903"/>
+<node TEXT="Why user-defined types?" ID="ID_942735195" CREATED="1617801399980" MODIFIED="1617801400357"/>
+<node TEXT="What do structures do?" ID="ID_1327091139" CREATED="1617801404399" MODIFIED="1617801404717"/>
+<node TEXT="Memory layout" ID="ID_1748958496" CREATED="1617801422211" MODIFIED="1617801422530"/>
+<node TEXT="Nested structures" ID="ID_1383195217" CREATED="1617801434412" MODIFIED="1617801434749"/>
+<node TEXT="Structure pointers" ID="ID_693507055" CREATED="1617801439057" MODIFIED="1617801439398"/>
+</node>
+<node TEXT="Summary" ID="ID_1757532196" CREATED="1617801444158" MODIFIED="1617801444612"/>
+</node>
+</node>
+</map>
